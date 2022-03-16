5a96a65b1f674ede67569faee42f9559785fe903
joao <joaoflach@gmail.com>
2022-03-16 17:05:28 -0300

    refatorado strings inseridas no inicio do arquivo

--- create-tests/insertTests.sh
+++ create-tests/insertTests.sh
@@ -17,37 +17,39 @@ else
 	tvar=$2
 fi
 
-r=$(grep -i ";; The first three lines of this file were inserted by DrRacket." $newfilename)
+config=';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Incluído pelo script insertTests.sh ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; \
+\
+(require racket/base) \
+(require racket/struct) \
+(require test-engine/racket-tests) \
+\
+;; Namespace para determinar se funções foram definidas: \
+\
+(define-namespace-anchor a) \
+(define ns (namespace-anchor->namespace a)) \
+\
+;; Arquivo de imagens de saida: \
+\
+(define $ARQUIVO '$imgfilename') \
+\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; \n'
 
-if [ -z $r ]
-then
-    echo "Arquivo NOT RACKETEADO"
-    sed -i '1 i\
-							\;; The first three lines of this file were inserted by DrRacket. They record metadata \
-							\;; about the language level of this file in a form that our tools can easily process. \
-							\#reader(lib "htdp-beginner-reader.ss" "lang")((modname b) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))\
-             \
-             ' $newfilename
-fi
+sed -i "1 i $config " $newfilename
 
-sed -i '4 i\
-             \;; Arquivo de imagens de saida: \
-            ' $newfilename
-sed -i '5 i\
-             \(define $ARQUIVO "'"$imgfilename"'") \
-             \
-            ' $newfilename
+rkt_config=';; The first three lines of this file were inserted by DrRacket. They record metadata \
+;; about the language level of this file in a form that our tools can easily process. \
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname b) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f))) \
+'
 
-
-sed -i '4 i\
-             \(require racket/base) \
-             \(require racket/struct) \
-             \(require test-engine/racket-tests) \
-             \;; Namespace para determinar se funções foram definidas: \
-             \(define-namespace-anchor a) \
-             \(define ns (namespace-anchor->namespace a)) \
-             \
-             ' $newfilename
+first_line=$(head -1 $newfilename)
+case $first_line in
+  (*";; The first three lines of this file were inserted by DrRacket. They record metadata"*)
+		echo "Aquivo RKT"
+     ;;
+  (*)
+		echo "Arquivo not RKT"
+		sed -i "1 i $rkt_config " $newfilename
+esac
 
 python3 struct-transparent.py $newfilename
 
@@ -56,9 +58,11 @@ python3 struct-transparent.py $newfilename
 first_line=$(head -1 $tvar)
 case $first_line in
   (*";; The first three lines of this file were inserted by DrRacket. They record metadata"*)
+		echo "entrou primeiro case"
      sed '1,3d' $tvar >> $newfilename
      ;;
   (*)
+		echo "entrou segundo case"
      cat $tvar >> $newfilename
 esac
 
